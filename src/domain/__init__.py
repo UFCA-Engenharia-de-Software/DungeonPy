5cdfd142a32ab47692ab96ab7817126da50e85e2
Leôncio Ferreira <leoncio.ferreira@aluno.ufca.edu.br>
2026-01-22 12:50:36 -0300

    style: aplicar formatação automática com ruff

--- src/domain/__init__.py
+++ src/domain/__init__.py
@@ -1,8 +1,8 @@
 """
 Módulo Domain - Núcleo de lógica do DungeonPy.
 
-Este módulo centraliza as entidades e objetos de valor que definem as 
-regras essenciais da masmorra, utilizando padrões de projeto para 
+Este módulo centraliza as entidades e objetos de valor que definem as
+regras essenciais da masmorra, utilizando padrões de projeto para
 gerenciar a complexidade do combate e estados.
 
 Componentes principais:

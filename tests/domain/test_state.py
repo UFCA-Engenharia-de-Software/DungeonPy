55551b68681b320c4c55f44548f4d02f5ec47a9b
cicero-jesus <cicero.jesus@aluno.ufca.edu.br>
2026-02-15 11:15:30 -0300

    refactor: modified the state and test_state for duration of decreases

--- tests/domain/test_state.py
+++ tests/domain/test_state.py
@@ -2,8 +2,8 @@ from domain.state import (
     NeutralState,
     PoisonedState,
     BurnState,
-    StunnedState,
     FrozenState,
+    StunnedState,
 )
 from domain.element import Element
 
@@ -11,12 +11,23 @@ from domain.element import Element
 class FakeEntity:
     """
     Fake class for State tests.
+    Simulates temporary modifiers.
     """
 
     def __init__(self):
         self.received_damage = []
-        self.attack = 10
-        self.speed = 10
+        self.base_attack = 10
+        self.base_speed = 10
+        self.attack_modifiers = []
+        self.speed_modifiers = []
+
+    @property
+    def attack(self):
+        return self.base_attack + sum(self.attack_modifiers)
+
+    @property
+    def speed(self):
+        return self.base_speed + sum(self.speed_modifiers)
 
     def damage_received(self, value, strike_element):
         self.received_damage.append((value, strike_element))
@@ -60,26 +71,54 @@ def test_poisoned_state_does_not_prevent_action():
     assert state.prevents_action() is False
 
 
-def test_burn_state_reduces_attack():
+def test_burn_state_applies_attack_modifier():
     state = BurnState(duration_turns=2, attack_decrease=3)
     entity = FakeEntity()
 
     state.apply_effect(entity)
 
     assert entity.attack == 7
+    assert -3 in entity.attack_modifiers
 
 
-def test_burn_state_reduces_duration():
-    state = BurnState(duration_turns=2, attack_decrease=3)
+def test_burn_state_restores_attack_after_expiration():
+    state = BurnState(duration_turns=1, attack_decrease=3)
     entity = FakeEntity()
 
     state.apply_effect(entity)
 
-    assert state.duration_turns == 1
+    assert entity.attack == 10
+    assert entity.attack_modifiers == []
 
 
 def test_burn_state_does_not_prevent_action():
     state = BurnState(duration_turns=1, attack_decrease=3)
+
+    assert state.prevents_action() is False
+
+
+def test_frozen_state_applies_speed_modifier():
+    state = FrozenState(duration_turns=2, speed_decrease=4)
+    entity = FakeEntity()
+
+    state.apply_effect(entity)
+
+    assert entity.speed == 6
+    assert -4 in entity.speed_modifiers
+
+
+def test_frozen_state_restores_speed_after_expiration():
+    state = FrozenState(duration_turns=1, speed_decrease=4)
+    entity = FakeEntity()
+
+    state.apply_effect(entity)
+
+    assert entity.speed == 10
+    assert entity.speed_modifiers == []
+
+
+def test_frozen_state_does_not_prevent_action():
+    state = FrozenState(duration_turns=1, speed_decrease=4)
 
     assert state.prevents_action() is False
 
@@ -97,27 +136,3 @@ def test_stunned_state_reduces_duration():
     state.apply_effect(entity)
 
     assert state.duration_turns == 1
-
-
-def test_frozen_state_reduces_speed():
-    state = FrozenState(duration_turns=2, speed_decrease=4)
-    entity = FakeEntity()
-
-    state.apply_effect(entity)
-
-    assert entity.speed == 6
-
-
-def test_frozen_state_reduces_duration():
-    state = FrozenState(duration_turns=2, speed_decrease=4)
-    entity = FakeEntity()
-
-    state.apply_effect(entity)
-
-    assert state.duration_turns == 1
-
-
-def test_frozen_state_does_not_prevent_action():
-    state = FrozenState(duration_turns=1, speed_decrease=4)
-
-    assert state.prevents_action() is False
